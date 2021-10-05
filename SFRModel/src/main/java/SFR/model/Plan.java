5df58f0d5987297301a61752fc10b96e7880121b
arnoldgq612 <arnoldgq612@gmail.com>
2021-10-05 16:49:38 -0600

    4.49pm

--- SFRModel/src/main/java/SFR/model/Plan.java
+++ SFRModel/src/main/java/SFR/model/Plan.java
@@ -1,6 +1,7 @@
 package sfr.model;
 
 import common.model.User;
+import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import javax.persistence.Column;
@@ -16,7 +17,7 @@ import org.hibernate.annotations.GenericGenerator;
  */
 @Entity
 @Table(name="T_Plan")
-public class Plan {
+public class Plan implements Serializable {
     
     @Id
     @Column(name = "PK_ROL")
@@ -51,129 +52,58 @@ public class Plan {
         this.commentList = new HashMap<>();
     }
 
-    
-    /**
-     * @return the id
-     */
     public int getId() {
         return id;
     }
 
-    /**
-     * @param id the id to set
-     */
     public void setId(int id) {
         this.id = id;
     }
-
-    /**
-     * @return the author
-     */
     public String getAuthor() {
         return author;
     }
-
-    /**
-     * @param author the author to set
-     */
     public void setAuthor(String author) {
         this.author = author;
     }
-
-    /**
-     * @return the name
-     */
     public String getName() {
         return name;
     }
-
-    /**
-     * @param name the name to set
-     */
     public void setName(String name) {
         this.name = name;
     }
-
-    /**
-     * @return the desc
-     */
     public String getDesc() {
         return desc;
     }
-
-    /**
-     * @param desc the desc to set
-     */
     public void setDesc(String desc) {
         this.desc = desc;
     }
-
-    /**
-     * @return the entryDate
-     */
     public Date getEntryDate() {
         return entryDate;
     }
-
-    /**
-     * @param entryDate the entryDate to set
-     */
     public void setEntryDate(Date entryDate) {
         this.entryDate = entryDate;
     }
-
-    /**
-     * @return the involvedList
-     */
     public HashMap<String, User> getInvolvedList() {
         return involvedList;
     }
-
-    /**
-     * @param involvedList the involvedList to set
-     */
     public void setInvolvedList(HashMap<String, User> involvedList) {
         this.involvedList = involvedList;
     }
-
-    /**
-     * @return the incidenceList
-     */
     public HashMap<String, Incidence> getIncidenceList() {
         return incidenceList;
     }
-
-    /**
-     * @param incidenceList the incidenceList to set
-     */
     public void setIncidenceList(HashMap<String, Incidence> incidenceList) {
         this.incidenceList = incidenceList;
     }
-
-    /**
-     * @return the riskList
-     */
     public HashMap<String, Risk> getRiskList() {
         return riskList;
     }
-
-    /**
-     * @param riskList the riskList to set
-     */
     public void setRiskList(HashMap<String, Risk> riskList) {
         this.riskList = riskList;
     }
-
-    /**
-     * @return the commentList
-     */
     public HashMap<String, Comment> getCommentList() {
         return commentList;
     }
-
-    /**
-     * @param commentList the commentList to set
-     */
     public void setCommentList(HashMap<String, Comment> commentList) {
         this.commentList = commentList;
     }

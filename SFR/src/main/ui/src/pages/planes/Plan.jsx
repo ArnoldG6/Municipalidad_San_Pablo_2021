8162c7cd0a5224f17992381fa2940cabbc593fa8
Esteban Ugalde <estalonso506@gmail.com>
2021-10-10 15:47:59 -0600

    pagina plan espe

---
+++ SFR/src/main/ui/src/pages/planes/Plan.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import {} from "react-bootstrap";
+class Plan extends React.Component {
+    render() { 
+        return <div></div>;
+    }
+}
+ 
+export default Plan;

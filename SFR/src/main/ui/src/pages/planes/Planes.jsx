7b7ec3f4ca8c93d98f41d3143ac2e8eba6921f10
tebyarce <earce1999@gmail.com>
2021-10-10 20:10:39 -0600

    plans table is ready, does need js work tho, add plan button and search bar done

--- SFR/src/main/ui/src/pages/planes/Planes.jsx
+++ SFR/src/main/ui/src/pages/planes/Planes.jsx
@@ -1,65 +1,96 @@
-import React, {Component} from 'react';
-import {Card, Nav, ListGroup, Row, Col, Table, Container, Link} from "react-bootstrap";
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import { Button, Nav, ListGroup, Row, Col, Table, Container, Modal } from "react-bootstrap";
 import './plans.css';
+
+
+
 class Planes extends Component {
+
+   
+
     render() {
-        return (           
-                <Row className="Content">
+        return (
+            <Row className="Content">
                 <Col md={1} sm={3} className="Sidebar">
 
-                <Nav defaultActiveKey="/home" className="flex-column">
-                    <Nav.Link href="/home">Proyectos</Nav.Link>
-                    <Nav.Link href="/home">Procesos</Nav.Link>
-                </Nav>
+                    <Nav defaultActiveKey="/home" className="flex-column">
+                        <Nav.Link href="/home">Proyectos</Nav.Link>
+                        <Nav.Link href="/home">Procesos</Nav.Link>
+
+                    </Nav>
 
                 </Col>
                 <Col md={11} sm={9}>
-                    
-                <Table hover>
-                            <thead>
-                                <tr>
+
+                    <Button id="NewItemButton" size="sm" onClick={this.openModal}>Crear Item</Button>
+                    <form action="" class="algo">
+                        <input type="text" id="fname" name="buscadfadfr" placeholder="Buscar"></input>
+                    </form>
+
+
+
+
+                    <Table hover>
+                        <thead>
+                            <tr>
                                 <th>Nombre</th>
                                 <th>ID</th>
                                 <th>Fecha</th>
                                 <th>Estado</th>
                                 <th>Autor</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                <td><a  href="/home">Ricardo antonio</a></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><a href="/home">Ricardo antonio</a></td>
                                 <td>Mark</td>
                                 <td>Otto</td>
                                 <td><div class="state">xD</div></td>
                                 <td>xD</td>
-                                </tr>
-                                <tr>
+                            </tr>
+                            <tr>
                                 <td>2</td>
                                 <td>Jacob</td>
                                 <td>Thornton</td>
                                 <td>@fat</td>
                                 <td>xD</td>
-                                </tr>
-                                
-                            </tbody>
-                            </Table>
+                            </tr>
 
-       
-                        
-                
+                        </tbody>
+                    </Table>
                 </Col>
-                </Row>
-                );
+
+                
+
+            </Row>
+        );
     }
 };
+
+function search() {
+    return 1;
+
+
+}
+
+
 
 
 
 export default Planes;
 /*
-<ListGroup variant="flush">
-                        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-                    </ListGroup> 
+var Plan = {nombre: 'xd', fecha: 123, estado: 'xd', autor: "Esteban1"};
+function addPlan() {
+    let request = new Request('http://localhost:3000/:/SFR/' + 'Plans', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(Plan)});
+    (async () => {
+        const response = await fetch(request);
+        if (!response.ok) {
+          
+            return;
+        }
+        document.location = 'http://localhost:8080/:/SFR/';
+    })();
+}
 */
+

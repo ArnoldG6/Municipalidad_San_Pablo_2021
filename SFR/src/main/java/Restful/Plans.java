9dc619fd3c15d13a9058af87b3a9726cf236f1cf
arnoldgq612 <arnoldgq612@gmail.com>
2021-10-12 16:33:26 -0600

    4.33pm testing the restful :p

--- SFR/src/main/java/Restful/Plans.java
+++ SFR/src/main/java/Restful/Plans.java
@@ -12,7 +12,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import sfr.dao.PlanDAO;
@@ -22,7 +21,7 @@ import sfr.model.Plan;
  *
  * @author jorsu
  */
-@Path("/API/Plans")
+@Path("/")
 public class Plans {
 
     @POST
@@ -57,7 +56,7 @@ public class Plans {
     }
 
     @GET
-    @Path("/asd")
+    //@Path("/listPlans")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Plan> getPlans() {
         return PlanDAO.getInstance().listAll();

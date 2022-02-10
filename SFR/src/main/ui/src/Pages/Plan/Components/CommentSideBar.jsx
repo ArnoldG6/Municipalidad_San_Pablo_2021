b4c3e70f39f5123bb34c126a8064894ef1dcf217
Haxiz <assassinsand@gmail.com>
2022-02-09 22:15:20 -0600

    More changes

--- SFR/src/main/ui/src/Pages/Plan/Components/CommentSideBar.jsx
+++ SFR/src/main/ui/src/Pages/Plan/Components/CommentSideBar.jsx
@@ -5,12 +5,15 @@ import './CommentSideBar.css';
 class CommentSideBar extends Component {
     render() {
         return (
-            <div className="plan-sidebar">
-                <h2 className="text-center mt-3">Comentarios</h2>
+            <div className="plan-sidebar flex-column">
+                <h2 className="text-center mt-3">
+                    Comentarios
+                </h2>
+                <hr />
                 <Nav className="navbar navbar-default" role="navigation">
                     <div className="side-menu-container">
                         <ul className="nav nav-pills flex-column mb-auto">
-                            
+
                         </ul>
                     </div>
                 </Nav>

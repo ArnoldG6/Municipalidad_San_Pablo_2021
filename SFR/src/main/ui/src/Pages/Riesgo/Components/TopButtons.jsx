617257b7f1b8c24f2cf159621e8406ec147088a9
ArnoldG6 <arnoldgq612@gmail.com>
2022-03-20 21:55:50 -0600

    Adding 'Specific Risk Page'

---
+++ SFR/src/main/ui/src/Pages/Riesgo/Components/TopButtons.jsx
@@ -0,0 +1,189 @@
+import React, { Component } from 'react';
+import './TopButtons.css';
+import { Stack, Button, OverlayTrigger, Tooltip } from "react-bootstrap";
+//import { toast } from 'react-toastify';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+class TopButtons extends Component {
+    constructor(props) {
+        super(props);
+        this.checkPermissions = this.checkPermissions.bind(this);
+    }
+
+    checkPermissions(toCheck) {
+        let perm = false;
+        cookies.get('roles', { path: process.env.REACT_APP_AUTH }).map((rol) => {
+            if (rol.description === toCheck) {
+                perm = true;
+                return true;
+            }
+            return false;
+        })
+        return perm;
+    }
+
+    render() {
+        let statusClass = this.props.status;
+        switch (statusClass) {
+            case 'Activo':
+                statusClass = 'success';
+                break;
+            case 'Inactivo':
+                statusClass = 'danger';
+                break;
+            case 'Completo':
+                statusClass = 'primary';
+                break;
+            default:
+                statusClass = 'secondary';
+                break;
+        }
+        return (
+            <div>
+                {/* Mobile */}
+                <div className='d-lg-none'>
+                    <Stack className="mt-4" direction="horizontal" gap={3}>
+                        {/* Editar */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Editar Plan
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant="outline-primary" onClick={this.props.openModalEdit} >
+                                <h2><i className="bi bi-pencil-square"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+
+                        {/* Eliminar */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Eliminar Plan
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant={this.checkPermissions("SUPER_ADMIN") ? "outline-danger" : "outline-dark"} onClick={this.props.openModalDelete} disabled={this.checkPermissions("SUPER_ADMIN") ? false : true}>
+                                <h2><i className="bi bi-trash"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+
+                        {/* Agregar Involucrado */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Agregar Involucrado
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant="outline-success">
+                                <h2><i className="bi bi-person-plus"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+
+                        {/* Matriz de Riesgos */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Matriz de Riesgos
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant="outline-warning">
+                                <h2><i className="bi bi-clipboard-data"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+                        <Button variant="light">
+                            <h2><i className="bi bi-download"></i></h2>
+                        </Button>
+                    </Stack>
+                </div>
+                {/* PC */}
+                <div className="d-none d-lg-block">
+                    <Stack className="mt-4" direction="horizontal" gap={3}>
+                        <Button variant="link" href="#/planes">
+                            <h5><i className="bi bi-chevron-left"></i> Volver</h5>
+                        </Button>
+                        {/* Editar */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Editar Plan
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant="outline-primary" onClick={this.props.openModalEdit} >
+                                <h2><i className="bi bi-pencil-square"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+
+                        {/* Eliminar */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Eliminar Plan
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant={this.checkPermissions("SUPER_ADMIN") ? "outline-danger" : "outline-dark"} onClick={this.props.openModalDelete} disabled={this.checkPermissions("SUPER_ADMIN") ? false : true}>
+                                <h2><i className="bi bi-trash"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+
+                        {/* Agregar Involucrado */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Agregar Involucrado
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant="outline-success">
+                                <h2><i className="bi bi-person-plus"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+
+                        {/* Matriz de Riesgos */}
+                        <OverlayTrigger
+                            delay={{ hide: 450, show: 300 }}
+                            overlay={(props) => (
+                                <Tooltip {...props}>
+                                    Matriz de Riesgos
+                                </Tooltip>
+                            )}
+                            placement="bottom"
+                        >
+                            <Button variant="outline-warning">
+                                <h2><i className="bi bi-clipboard-data"></i></h2>
+                            </Button>
+                        </OverlayTrigger>
+
+
+                        <Button className="ms-auto" variant="light">
+                            <h2><i className="bi bi-download"></i></h2>
+                        </Button>{' '}
+                        <div className="vr" />
+                        <Button variant={statusClass}>{this.props.status}</Button>{' '}
+                    </Stack>
+                </div>
+            </div>
+        );
+    }
+};
+export default TopButtons;
--- SFR/src/main/ui/src/Pages/Riesgo/Riesgo.jsx
+++ SFR/src/main/ui/src/Pages/Riesgo/Riesgo.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Row, Card, Nav, Table, Container, Col } from "react-bootstrap";
+import TopButtons from './Components/TopButtons';
 //import Cookies from 'universal-cookie';
 //const cookies = new Cookies();
 
-class Plan extends Component {
+export default class Plan extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -37,9 +39,10 @@ class Plan extends Component {
                 }, () => {
                     if (this.state.risk === null || typeof this.state.risk === 'undefined') {
                         this.props.history.push('/riesgos');
-                    } else {
-                        console.log(this.state.risk)
-                    }
+                    } //else {
+                    //console.log(this.state.risk)
+
+                    //}
                 });
             }).catch(error => {
                 this.props.history.push('/riesgos');
@@ -47,10 +50,108 @@ class Plan extends Component {
     }
 
     render() {
+        //let tableData = this.tableAssign();
+        let tableData = "some wea"
+        let a = this
         return (
-            <div>
+
+            <div className="Plan-Container">
+                {/* Mobile */}
+                <div className='d-lg-none container-fluid'>
+                    <Row>
+                        <TopButtons
+                            openModalEdit={this.openModalEdit}
+                            openModalDelete={this.openModalDelete}
+                            status={"Some wea"} />
+                    </Row>
+                    {/* Datos del Plan */}
+                    <Row className="mt-4">
+                        {
+                            (this.state.risk === null || typeof this.state.risk === 'undefined') ?
+                                <h1>Cargando Datos</h1> :
+                                <div>
+                                    <h1>{this.state.risk.name}</h1>
+                                    <h2>{this.state.risk.id}</h2>
+                                    <h2>{this.state.risk.generalType}</h2>
+                                    <h4>{this.state.risk.areaType}</h4>
+                                    <h4>{this.state.risk.specType}</h4>
+                                    <p>{this.state.risk.factors}</p>
+                                </div>
+                        }
+                    </Row>
+                    <Card id="card">
+                        <Card.Header>
+                            <Nav fill variant="tabs" defaultActiveKey="riesgosTab">
+                                <Nav.Item>
+                                    <Nav.Link eventKey="riesgosTab">Riesgos</Nav.Link>
+                                </Nav.Item>
+                            </Nav>
+                        </Card.Header>
+                        <Card.Body>
+                            {tableData}
+                        </Card.Body>
+                    </Card>
+                </div>
+                {/* Vista Desktop */}
+                <div className="d-none d-lg-block">
+                    <div className="container-fluid Data-Container">
+                        {/* Botones de uso en el Plan */}
+                        <Row>
+                            <TopButtons
+                                openModalEdit={this.openModalEdit}
+                                openModalDelete={this.openModalDelete}
+                                status={"status"} />
+                        </Row>
+                        {/* Datos del Plan */}
+                        <Row className="mt-4">
+                            {
+                                (this.state.risk === null || typeof this.state.risk === 'undefined') ?
+                                    <h1>Cargando Datos</h1> :
+                                    <div>
+                                        <h1>{this.state.risk.id}</h1>
+                                        <h1>{this.state.risk.name}</h1>
+                                        <Container>
+                                            <Row>
+                                                <Col>
+                                                    <Card>
+                                                        <Card.Body>
+                                                            <Card.Title>Sobre Este Riesgo</Card.Title>
+                                                            <Card.Text>
+                                                                <p>Este riesgo aparece en ....</p>
+                                                                <p>Este riesgo aparece en ....</p>
+                                                                <p>Este riesgo aparece en ....</p>
+                                                            </Card.Text>
+                                                        </Card.Body>
+                                                    </Card>
+                                                </Col>
+                                                <Col>
+                                                    <Card>
+                                                        <Card.Body>
+                                                            <Card.Title class = "text-center">
+                                                                <h2>Información Específica</h2>
+                                                            </Card.Title>
+                                                            <Table striped bordered hover responsive="md">
+                                                                <tbody>
+                                                                    <tr><td><b>Tipo General</b></td><td>{this.state.risk.generalType}</td></tr>
+                                                                    <tr><td><b>Tipo Por Área</b></td><td>{this.state.risk.areaType}</td></tr>
+                                                                    <tr><td><b>Tipo Específico</b></td><td>{this.state.risk.specType}</td></tr>
+                                                                    <tr><td><b>Probabilidad</b></td><td>{this.state.risk.probability}</td></tr>
+                                                                    <tr><td><b>Impacto</b></td><td>{this.state.risk.impact}</td></tr>
+                                                                    <tr><td><b>Magnitud</b></td><td>{this.state.risk.magnitude}</td></tr>
+                                                                </tbody>
+                                                            </Table>
+                                                        </Card.Body>
+                                                    </Card>
+                                                </Col>
+                                            </Row>
+                                        </Container>
+                                    </div>
+                            }
+                        </Row>
+                    </div>
+                </div>
             </div>
         );
     }
 }
-export default Plan;
+

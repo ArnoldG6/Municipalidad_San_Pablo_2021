a0d79f649bfd7ec077ba032889563ae03843c322
ArnoldG6 <arnoldgq612@gmail.com>
2022-03-23 20:45:17 -0600

    Adding folder '.bat scripts' which includes three scripts that auto-initialize the front-end clients. Also, both '.env' files of the front-end React clients were changed with @param PORT

---
+++ .bat scripts/start_frontend_cllients.bat
@@ -0,0 +1,6 @@
+::Author: Arnoldo José González Quesada. 
+::Contact me via: arnoldg612@gmail.com
+::Github username: ArnoldG6
+::Use this script in Windows environments in order to start React frontend clients for testing.
+start .\start_auth_client.bat
+start .\start_sfr_client.bat
---
+++ .bat scripts/start_sfr_client.bat
@@ -0,0 +1,5 @@
+::Author: Arnoldo José González Quesada. 
+::Contact me via: arnoldg612@gmail.com
+::Github username: ArnoldG6
+cd ..\SFR\src\main\ui
+npm start

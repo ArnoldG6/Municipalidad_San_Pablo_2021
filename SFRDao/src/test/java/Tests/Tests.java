e6cd94454da333961eeb921bde10436f4be6a7f0
ArnoldG6 <arnoldgq612@gmail.com>
2022-04-05 14:50:08 -0600

    DB directory cleaning

--- SFRDao/src/test/java/Tests/Tests.java
+++ SFRDao/src/test/java/Tests/Tests.java
@@ -22,10 +22,7 @@ public class Tests {
     @Test
     public void main() {
         try {
-            Risk r = RiskDAO.getInstance().searchById(2);
-            Incidence i = new Incidence("name", "description", new Date(), 5, "cause", r);
-            System.out.println(i.toString());
-            IncidenceDAO.getInstance().add(i);
+            System.out.println(RiskDAO.getInstance().listAllHMBbyID());
         } catch (Exception ex) {
             Logger.getLogger(Tests.class.getName()).log(Level.SEVERE, null, ex);
         }

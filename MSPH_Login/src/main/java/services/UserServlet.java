332701cd56dc2bf6a41514062fcf3c0d02328ea4
tebyarce <earce1999@gmail.com>
2022-04-27 09:22:16 -0600

    User servlet

--- MSPH_Login/src/main/java/services/UserServlet.java
+++ MSPH_Login/src/main/java/services/UserServlet.java
@@ -1,10 +1,14 @@
-/**
- * @author ArnoldG6
- * AuthServlet class is in charge of handling requests from the clients
- * that require authorization.
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package services;
 
+/**
+ *
+ * @author tebya
+ */
 import com.google.gson.Gson;
 import common.dao.UserDAO;
 import common.model.User;
@@ -20,12 +24,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.json.JSONObject;
 
-@WebServlet(name = "AuthServlet",
+@WebServlet(name = "UserServlet",
         urlPatterns = {
-            "/API/Auth"
+            "/API/User"
         }
 )
-public class AuthServlet extends HttpServlet {
+public class UserServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
@@ -35,35 +39,37 @@ public class AuthServlet extends HttpServlet {
         response.setContentType("application/json");
         try {
             switch (request.getServletPath()) {
-                case "/API/Auth":
-                    authUser(request, response);
+                case "/API/User/":
+                    getUser(request, response);
                     break;
+                case "/API/editUser": break;
                 
                 //case "/API/ExpireSession": expireSession(request,response); break;
             }
         } catch (IOException | ServletException ex) {
             System.err.println(ex);
-            Logger.getLogger(AuthServlet.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
             throw ex;
         }
     }
 
     // <editor-fold defaultstate="collapsed" desc="Auth methods.">
-    private void authUser(HttpServletRequest request, HttpServletResponse response)
+    private void getUser(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try{
             response.setContentType("application/json");
             response.setCharacterEncoding("UTF-8");
             JSONObject responseJSON = new JSONObject();
             JSONObject requestJSON = new JSONObject(request.getReader().lines().collect(Collectors.joining()));
-            User u = UserDAO.getInstance().userAuth(requestJSON.getString("username"), requestJSON.getString("pwd"));
+            User u = UserDAO.getInstance().searchById(Integer.valueOf(requestJSON.getString("username")));
             if (u == null) throw new AuthException();
-            responseJSON.put("authStatus", true);
             responseJSON.put("username", String.valueOf(u.getIdUser()));
             responseJSON.put("full_name", u.getOfficial().getName() + " " + u.getOfficial().getSurname());
             responseJSON.put("roles", u.getRoles());
+            responseJSON.put("department", u.getOfficial().getDepartment().getDescription());
             responseJSON.put("token", "xd");
             response.getWriter().write(responseJSON.toString());
+            
         }catch(AuthException e){
             response.getWriter().write(e.jsonify());
         }finally{
@@ -71,7 +77,6 @@ public class AuthServlet extends HttpServlet {
             response.getWriter().close();
         }
             
-
     }
 
     //private void expireSession(HttpServletRequest request, HttpServletResponse response) 
@@ -82,7 +87,7 @@ public class AuthServlet extends HttpServlet {
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods.">
     @Override
     public String getServletInfo() {
-        return "Auth Servlet. Do not try to attack it. :)";
+        return "User Servlet. Do not try to attack it. :)";
     }
 
     @Override
@@ -91,7 +96,7 @@ public class AuthServlet extends HttpServlet {
         try {
             processRequest(request, response);
         } catch (Exception ex) {
-            Logger.getLogger(AuthServlet.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -101,8 +106,19 @@ public class AuthServlet extends HttpServlet {
         try {
             processRequest(request, response);
         } catch (Exception ex) {
-            Logger.getLogger(AuthServlet.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{   
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
     }
 
     @Override
@@ -110,12 +126,12 @@ public class AuthServlet extends HttpServlet {
             throws ServletException, IOException {
         try {
             response.addHeader("Access-Control-Allow-Origin", "*");
-            response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Userorization");
             response.addHeader("Access-Control-Allow-Credentials", "true");
             response.addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,HEAD");
             //processRequest(request, response);
         } catch (Exception ex) {
-            Logger.getLogger(AuthServlet.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
    

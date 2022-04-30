e66e309e1b133cb2999c5f87e89abafa92e15105
ArnoldG6 <arnoldgq612@gmail.com>
2022-04-30 14:34:04 -0600

    ....token.sql little change

--- ANNEXES/DB/password_reset_token_event.sql
+++ ANNEXES/DB/password_reset_token_event.sql
@@ -1,8 +1,8 @@
 -- Author: "Arnoldo González Quesada".
 -- Contact me via: "arnoldgq612@gmail.com".
 -- GitHub username: "ArnoldG6".
-use si_db;
-drop event delete_exp_codes;
+USE si_db;
+DROP EVENT IF EXISTS delete_exp_codes;
 CREATE EVENT delete_exp_codes
 ON SCHEDULE EVERY 30 MINUTE
 ON COMPLETION PRESERVE

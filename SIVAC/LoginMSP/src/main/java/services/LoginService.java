f3c6430f4a98f98f816908358fea01ed9af71f98
ArnoldG6 <arnoldgq612@gmail.com>
2022-06-10 14:59:46 -0600

    Fix on redirection

--- SIVAC/LoginMSP/src/main/java/services/LoginService.java
+++ SIVAC/LoginMSP/src/main/java/services/LoginService.java
@@ -10,29 +10,35 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import model.Model;
-import common.model.User;
+import java.util.stream.Collectors;
+import org.json.JSONObject;
 
 public class LoginService extends HttpServlet {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+        protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
         try {
-            //could be username or email
-            String username = (String)request.getParameter("username");
-            String pwd = (String) request.getParameter("pwd");
-            User user = Model.getInstance().searchUser(username, pwd);
-            HttpSession session = request.getSession(true);
-            session.setAttribute("user", user);
-            request.getRequestDispatcher("/Pagina1.jsp").forward(request, response);
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS,HEAD");
+            handleFriendlyRedict(request, response);
+
         } catch (Exception ex) {
             System.err.printf("%s\n", ex.getMessage());
-            response.sendRedirect("index.jsp");
         }
 
     }
+
+    private void handleFriendlyRedict(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        JSONObject requestJSON = new JSONObject(request.getReader().lines().collect(Collectors.joining()));
+        //User u = UserDAO.getInstance().searchById(requestJSON.getInt("username"));
+        request.getSession().setAttribute("id", requestJSON.getInt("username"));
+        request.getRequestDispatcher("moduloSivac/MainScreen.jsp").forward(request, response);
+    }
+
+            //request.getRequestDispatcher("/Pagina1.jsp").forward(request, response);
+
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
